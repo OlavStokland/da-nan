ef201349d5ecb5e8ad23b8f2bebc8db392b184d8
OlavStokland <82805310+OlavStokland@users.noreply.github.com>
2022-04-30 11:15:54 +0200

    Lagt til kommentarer

--- mp4/opprett_database.sql
+++ mp4/opprett_database.sql
@@ -26,10 +26,10 @@ CREATE TABLE IF NOT EXISTS Dikt (
 /* sha512sum - idiot */
 INSERT INTO Bruker(epost,passordhash,fornavn,etternavn)
 VALUES( 'olavstokk@danan.no','05331e25d57db8dcaf36507557f7516970f2ad3eae1ee37fb0d2845cad60f21f97cdae72bbb14e285719cedd2a3dde67ec0392fb831ba7072c6049c4aa69ecd8  -','olav','stokland');
-
+/* sha512sum - foynhagen */
 INSERT INTO Bruker(epost,passordhash,fornavn,etternavn)
 VALUES( 'espend@danan.no','9899053c4e70d897c9cbb826be62c71904d6b3850686cf93cdcb5e7518243d6686841d69992012a35753d03757812d5b06ab8c70af0e4218158bfc4666ba2cfb  -','espen','disch');
-
+/*Eksempeldata*/
 INSERT INTO Dikt(diktID,dikt,epost)
 VALUES(1, 'Det heiter ikkje: eg – no lenger.Heretter heiter det: vi.Eig du lykka så er ho ikkje lengerberre di.Alt det som bror din kan ta imot av lykka di, må du gi.','olavstokk@danan.no');
 
